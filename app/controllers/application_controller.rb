03a28d9be4c3aa3fc6bd6c1f625d2e7e7ef95310
James Parrish <jthomasparrishjr@gmail.com>
2022-12-28 22:39:33 -0600

    updated routes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,4 +6,21 @@ class ApplicationController < Sinatra::Base
     { message: "Good luck with your project, Big Jim!" }.to_json
   end
 
+  get '/dogs' do
+    dogs = Pet.where(species: "dog") 
+    dogs.to_json
+  end
+
+  get '/cats' do
+    cats = Pet.where(species: "cat")
+    cats.to_json
+  end
+
+  get '/comments' do
+    comments = Comment.all
+    comments.to_json
+  end
+
+  
+
 end
